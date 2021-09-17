c021507930b463e4e891901dec8ae2bd3152676b
Alexander <stos@self.team>
2021-09-17 17:36:57 +0300

    //

--- Source/Charts/Renderers/LineScatterCandleRadarRenderer.swift
+++ Source/Charts/Renderers/LineScatterCandleRadarRenderer.swift
@@ -37,7 +37,7 @@ open class LineScatterCandleRadarRenderer: BarLineScatterCandleBubbleRenderer
             context.strokePath()
             context.setFillColor(NSUIColor.white.cgColor)
             context.fillEllipse(in: CGRect(x: point.x - 6, y: point.y - 6, width: 12, height: 12))
-            context.setFillColor(NSUIColor(rgb: 0x2B61DF).cgColor)
+            context.setFillColor(NSUIColor(red: 43/255, green: 97/255, blue: 223/255, alpha: 1).cgColor)
             context.fillEllipse(in: CGRect(x: point.x - 4, y: point.y - 4, width: 8, height: 8))
         }
         
